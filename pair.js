a46a011a5c778bc5f2cf4c26a1fe9ee201a5661a
Jayden-official <cutesoul0731@gmail.com>
2025-07-24 12:20:16 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -59,7 +59,7 @@ router.get('/', async (req, res) => {
                     const session = await client.sendMessage(client.user.id, { text: '' + b64data });
 
                     // Send message after session
-                    await client.sendMessage(client.user.id, {text: "```Smart has been linked to your WhatsApp account! Do not share this session_id with anyone.\n\nCopy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nIncase you are facing Any issue reach me via here👇\n\nhttps://wa.me/254794597254\n\nAnd don't forget to sleep😴, for even the rentless must recharge⚡.\n\nGoodluck 🎉. ```" }, { quoted: session });
+                    await client.sendMessage(client.user.id, {text: "```J-md has been linked to your WhatsApp account! Do not share this session_id with anyone.\n\nCopy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nIncase you are facing Any issue reach me via here👇\n\nhttps://wa.me/254794597254\n\nAnd don't forget to sleep😴, for even the rentless must recharge⚡.\n\nGoodluck 🎉. ```" }, { quoted: session });
                     
                     await delay(100);
                     await client.ws.close();
